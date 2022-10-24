2bdd29929953670c2a33d3854d802c7f3be407b8
user03 <zaika0180@gmail.com>
2022-10-24 13:05:11 +0300

    pr13

---
+++ app/src/main/java/com/example/pr13/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.pr13
+
+import android.app.ProgressDialog.show
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast.makeText
+import java.time.Duration
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText()
+        show()
+    }
+}
+class Toast{
+    fun makeText(context: Context, text:String, duration:Int){
+
+    }
+
+}
