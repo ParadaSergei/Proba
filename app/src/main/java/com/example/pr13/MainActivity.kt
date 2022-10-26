3c6f652a7a0bd9d0c35360a1aaa4e3193b03a90e
Парада Сергей Сергеевич <106469820+ParadaSergei@users.noreply.github.com>
2022-10-26 20:24:13 +0300

    Add files via upload

--- app/src/main/java/com/example/pr13/MainActivity.kt
+++ app/src/main/java/com/example/pr13/MainActivity.kt
@@ -4,20 +4,33 @@ import android.app.ProgressDialog.show
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import android.widget.Toast.makeText
 import java.time.Duration
 
 class MainActivity : AppCompatActivity() {
+    val editText1:EditText = findViewById(R.id.editText1)
+    val editText22:EditText = findViewById(R.id.editText22)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText()
-        show()
+    }
+
+    fun sum(view: View) {
+
+        editText1.requestFocus()
+    }
+
+    fun konvert(editText1:EditText,editText22:EditText) {
+        var summa:Int = editText1-editText22
     }
 }
+
+
+
 class Toast{
     fun makeText(context: Context, text:String, duration:Int){
 
     }
-
 }
